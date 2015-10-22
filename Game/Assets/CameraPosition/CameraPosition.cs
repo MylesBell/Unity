944cbf7d7958194d66c9f88b13bec572b866fee1
Grzegorz\ Pawelczak <gp13823@my.bristol.ac.uk>
2015-10-22 11:00:07 +0100

    Add networked Splashscreen Add multiple screens camera aligment

---
+++ Game/Assets/CameraPosition/CameraPosition.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class CameraPosition : MonoBehaviour {
+    public float speed;
+
+    private bool isServer;
+
+    
+
+	// Use this for initialization
+	void Start () {
+        int screenNumber = PlayerPrefs.GetInt("screen", 0);
+        isServer = PlayerPrefs.GetInt("isServer", 1) == 1 ? true : false;
+        Debug.Log("Screen Number: " + screenNumber);
+
+        float height = 2f * Camera.main.orthographicSize; //the height of the screen in the game
+        float width = height * Camera.main.aspect; //the width of the screen in the game 
+        Vector3 v3 = transform.position; //get current pos
+        v3.x = width / 2 + width*screenNumber; //offset the camera correctly
+        transform.position = v3;
+
+    }
+
+    // Update is called once per frame
+  
+
+    void LateUpdate()
+    {
+        if (isServer)
+        {
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
+            }
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                transform.Translate(new Vector3(-speed * Time.deltaTime, 0, 0));
+            }
+        }
+
+    }
+}
---
+++ Game/Assets/Networking/GraniteNetworkManager.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class GraniteNetworkManager : NetworkManager {
+    public InputField IPAddressInput;
+    public InputField PortNumberInput;
+    public InputField ScreenNumberInput;
+
+    public void StartupHost()
+    {
+        SetIPAddress();
+        SetPort();
+        PlayerPrefs.SetInt("screen", 0);
+        PlayerPrefs.SetInt("isServer", 1);
+        NetworkManager.singleton.StartHost();
+    }
+
+    public void JoinScreen()
+    {
+        SetIPAddress();
+        SetPort();
+        SetScreen();
+        SetLane();
+        PlayerPrefs.SetInt("isServer", 0);
+        NetworkManager.singleton.StartClient();
+    }
+    public void SetIPAddress()
+    {
+        string ipAddress = IPAddressInput.text;
+        Debug.Log("IP Address is: " + ipAddress);
+        NetworkManager.singleton.networkAddress = ipAddress;
+    }
+
+    public void SetPort()
+    {
+        string portNumber = PortNumberInput.text;
+        int port = -1;
+        int.TryParse(portNumber, out port);
+        Debug.Log("Port Number is: " + port);
+        NetworkManager.singleton.networkPort = port;
+    }
+    public void SetLane()
+    {
+
+    }
+    public void SetScreen()
+    {
+        string screenNumber = ScreenNumberInput.text;
+        Debug.Log("Screen Number is: " + screenNumber);
+        int screen = -1;
+        int.TryParse(screenNumber, out screen);
+        PlayerPrefs.SetInt("screen", screen);
+    }
+}
