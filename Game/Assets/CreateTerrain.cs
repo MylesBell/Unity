fc392c6995623182443031b945a8ff87ef91c74e
oscarrobertson] <or13827@bristol.ac.uk>
2015-11-19 15:23:21 +0000

    Ensure low poly works with NetworkedGame scene

--- Game/Assets/CreateTerrain.cs
+++ Game/Assets/CreateTerrain.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.Networking;
 
-public class CreatTerrain2 : NetworkBehaviour
+public class CreateTerrain : NetworkBehaviour
 {
 
     public GameObject[] laneSegments;
