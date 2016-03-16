af15d09c8007d039f6d60c2be68f196c7b038c18
Myles Bell <myles.bell@hotmail.co.uk>
2016-03-16 16:47:48 +0000

    play specials at good height above characters

--- Game/Assets/Scripts/GruntAndHero/Specials/Specials.cs
+++ Game/Assets/Scripts/GruntAndHero/Specials/Specials.cs
@@ -76,7 +76,7 @@ public class Specials : NetworkBehaviour, IPlayerSpecial {
         
         Special special = specialObject.GetComponent<Special>();
         special.transform.parent = gameObject.transform;
-        float height =  gameObject.GetComponent<Collider>().bounds.size.y/2;
+        float height =  gameObject.GetComponent<BoxCollider>().size.y;
         special.InitialiseSpecial(height);
         
         return special;
