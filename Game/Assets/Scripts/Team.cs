01624a627a4ff2625ceb350588e6c0af9b6c5d3c
Louis Ditzel <louisditzel@gmail.com>
2016-04-12 18:44:00 +0100

    Update scripts to point to new classes

--- Game/Assets/Scripts/Team.cs
+++ Game/Assets/Scripts/Team.cs
@@ -25,7 +25,7 @@ public class Team : NetworkBehaviour {
     private float zPositionOffsetLeft;
     private int numberOfChannels;
 
-    public GameObject HeroPrefab;
+    public GameObject[] HeroPrefabs;
     public GameObject BasePrefab;
     public GameObject GruntPrefab;
 
@@ -133,7 +133,8 @@ public class Team : NetworkBehaviour {
 	}
 
     public void CreatePlayer(string playerID, string playerName) {
-        GameObject hero = unitFactory.CreateHero(HeroPrefab);
+        int index = Random.Range(0,HeroPrefabs.Length);
+        GameObject hero = unitFactory.CreateHero(HeroPrefabs[index]);
         
         hero.GetComponent<Hero>().InitialiseGameObject(this);        
 		hero.GetComponent<Hero>().setplayerID (playerID);
