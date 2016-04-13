4d05eacb5909b3932a3ef26047ff661a613b9651
Myles Bell <myles.bell@hotmail.co.uk>
2016-04-13 12:25:43 +0100

    staring attack effect setup

---
+++ Game/Assets/Scripts/GruntAndHero/AllPlays/AttackEffect.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class AttackEffect : AllPlay {
+        
+    override public void Initialise(float height){
+        transform.localPosition = new Vector3(0,height,0);
+    }
+    
+    override public void Upgrade(){
+        
+    }
+    
+    override public void Reset(){
+        
+    }
+    
+    override public void Use(params float[] inputs){   
+        gameObject.SetActive(true);
+        RpcPlayEffect();
+    }
+    
+    [ClientRpc]
+    private void RpcPlayEffect() {
+        gameObject.SetActive(true);
+        StartCoroutine(PlayEffect());
+    }
+    
+    IEnumerator PlayEffect(){
+        yield return new WaitForSeconds(1);
+        gameObject.SetActive(false);
+    }
+}
